ef385bbe2ed0edd6e4442a5fbac9b35fefbdf7de
cagatayakbulut <57892370+cagatayakbulut@users.noreply.github.com>
2026-05-17 20:55:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-def get_weather(city):
+def get_weather(lat, lon, name):
     url = "https://api.openweathermap.org/data/2.5/weather"
     params = {
-        "q": city,
+        "lat": lat,
+        "lon": lon,
         "appid": OPENWEATHER_API_KEY,
         "units": "metric",
         "lang": "tr"
@@ -17,31 +18,18 @@ def get_weather(city):
     response = requests.get(url, params=params)
     data = response.json()
 
+    if "main" not in data:
+        raise Exception(f"Hava durumu alınamadı: {data}")
+
     return {
+        "name": name,
         "temp": data["main"]["temp"],
-        "desc": data["weather"][0]["description"]
+        "desc": data["weather"][0]["description"],
+        "wind": data["wind"]["speed"]
     }
 
-kadikoy = get_weather("Kadikoy,TR")
-sakarya = get_weather("Sakarya,TR")
+kadikoy = get_weather(40.9919, 29.0252, "Kadıköy")
+sakarya = get_weather(40.7731, 30.3948, "Sakarya")
+izmit = get_weather(40.7654, 29.9408, "İzmit / Kocaeli")
 
-message = f"""
-Günaydın Çağatay ☀️
-
-Kadıköy:
-{kadikoy['temp']:.0f}°C - {kadikoy['desc']}
-
-Sakarya:
-{sakarya['temp']:.0f}°C - {sakarya['desc']}
-
-Şemsiye durumunu kontrol etmeyi unutma ☔
-"""
-
-url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-
-payload = {
-    "chat_id": TELEGRAM_CHAT_ID,
-    "text": message
-}
-
-requests.post(url, json=payload)
+risk_words = ["yağmur", "sağanak", "kar", "fırtına", "dolu", "gök gürültülü
